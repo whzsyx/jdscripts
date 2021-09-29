2a7e5a9cbf7a7d2f32911ac02ffae253eeda2e6b
whzsyx <3377107425@qq.com>
2021-09-30 00:21:24 +0800

    Update jd_unsubscribe_xh.js

--- jd_unsubscribe_xh.js
+++ jd_unsubscribe_xh.js
@@ -5,6 +5,7 @@
  * @LastEditors: X1a0He
  * @Description: 批量取关京东店铺和商品
  * @Fixed: 不再支持Qx，仅支持Node.js
+cron 22 22 * * * jd_unsubscribe_xh.js
  */
 const $ = new Env('批量取关店铺和商品');
 //Node.js用户请在jdCookie.js处填写京东ck;
